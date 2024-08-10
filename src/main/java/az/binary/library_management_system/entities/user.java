7730b59c665c79bef0c8e7ccaca72d170e87767c
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2024-08-11 00:05:44 +0400

    added user entity

---
+++ src/main/java/az/binary/library_management_system/entities/user.java
@@ -0,0 +1,27 @@
+package az.binary.library_management_system.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class user {
+
+    private int id;
+    private String first_name;
+    private String last_name;
+    private Email emailAddress;
+    private String password;
+
+
+}
