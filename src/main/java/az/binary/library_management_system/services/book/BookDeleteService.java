fa641bfdab6ac14cef38b3890935ef06df2e053e
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2024-08-15 00:21:21 +0400

    created book delete service

--- src/main/java/az/binary/library_management_system/services/book/BookDeleteService.java
+++ src/main/java/az/binary/library_management_system/services/book/BookDeleteService.java
@@ -1,5 +1,6 @@
 package az.binary.library_management_system.services.book;
 
+import az.binary.library_management_system.dto.requests.book.BookDeleteRequest;
 import az.binary.library_management_system.mappers.BookMapper;
 import az.binary.library_management_system.repositories.BookRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,5 +12,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class BookDeleteService {
     private final BookRepository bookRepository;
-    private final BookMapper bookMapper;
+
+    public void deleteAll(){
+        bookRepository.deleteAll();
+    }
+
+    public void deleteById(BookDeleteRequest deleteRequest){
+        bookRepository.deleteById(deleteRequest.getId());
+    }
 }
