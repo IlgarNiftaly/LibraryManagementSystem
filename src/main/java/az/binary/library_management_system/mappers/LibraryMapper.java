23edeb5b741452b06b9811ebca7b3a2c9f8ac122
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2024-08-14 18:43:25 +0400

    added library read service readAll method

--- src/main/java/az/binary/library_management_system/mappers/LibraryMapper.java
+++ src/main/java/az/binary/library_management_system/mappers/LibraryMapper.java
@@ -2,6 +2,7 @@ package az.binary.library_management_system.mappers;
 
 import az.binary.library_management_system.dto.requests.library.LibraryCreateRequest;
 import az.binary.library_management_system.dto.responses.library.LibraryCreateResponse;
+import az.binary.library_management_system.dto.responses.library.LibraryReadResponse;
 import az.binary.library_management_system.entities.Library;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -11,5 +12,6 @@ public interface LibraryMapper {
     Library mapRequestToEntity(LibraryCreateRequest createRequest);
 
     LibraryCreateResponse mapCreateToResponse(Library library);
+    LibraryReadResponse mapReadToResponse(Library library);
 
 }
---
+++ src/main/java/az/binary/library_management_system/services/library/LibraryReadService.java
@@ -0,0 +1,25 @@
+package az.binary.library_management_system.services.library;
+
+import az.binary.library_management_system.dto.requests.library.LibraryReadRequest;
+import az.binary.library_management_system.dto.responses.library.LibraryReadResponse;
+import az.binary.library_management_system.mappers.LibraryMapper;
+import az.binary.library_management_system.repositories.LibraryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LibraryReadService {
+    private final LibraryRepository libraryRepository;
+    private final LibraryMapper libraryMapper;
+
+    public List<LibraryReadResponse> readAll(){
+        return libraryRepository.findAll().stream()
+                .map(libraryMapper::mapReadToResponse)
+                .toList();
+    }
+}
