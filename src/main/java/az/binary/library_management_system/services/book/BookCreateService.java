28ad98f550a1a1785b779442b27b0abfe66fb732
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2024-08-15 00:19:46 +0400

    created book create service

--- src/main/java/az/binary/library_management_system/services/book/BookCreateService.java
+++ src/main/java/az/binary/library_management_system/services/book/BookCreateService.java
@@ -1,4 +1,24 @@
 package az.binary.library_management_system.services.book;
 
+import az.binary.library_management_system.dto.requests.book.BookCreateRequest;
+import az.binary.library_management_system.dto.responses.book.BookCreateResponse;
+import az.binary.library_management_system.entities.Book;
+import az.binary.library_management_system.mappers.BookMapper;
+import az.binary.library_management_system.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class BookCreateService {
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    public BookCreateResponse create(BookCreateRequest createRequest){
+        Book book = bookMapper.mapRequestToEntity(createRequest);
+        bookRepository.save(book);
+        return bookMapper.mapCrateToResponse(book);
+    }
 }
--- src/main/java/az/binary/library_management_system/services/book/BookUpdateService.java
+++ src/main/java/az/binary/library_management_system/services/book/BookUpdateService.java
@@ -1,4 +1,15 @@
 package az.binary.library_management_system.services.book;
 
+import az.binary.library_management_system.mappers.BookMapper;
+import az.binary.library_management_system.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class BookUpdateService {
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 }
