1e161c41d279edfe3b09bfea49be9225989fc833
IlgarNiftaly <158113662+IlgarNiftaly@users.noreply.github.com>
2024-08-14 17:54:12 +0400

    added library create request

--- src/main/java/az/binary/library_management_system/dto/requests/library/LibraryCreateRequest.java
+++ src/main/java/az/binary/library_management_system/dto/requests/library/LibraryCreateRequest.java
@@ -1,4 +1,15 @@
 package az.binary.library_management_system.dto.requests.library;
 
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LibraryCreateRequest {
+
+    private String name;
+    private String status;
+
 }
